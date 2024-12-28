505c326bb2da7b3898ceeb569c01533754a9ed6d
zhuang00 <zhuang2011@gmail.com>
2024-12-28 17:22:43 +0800

    Update build_action.sh

--- build_action.sh
+++ build_action.sh
@@ -5,7 +5,7 @@ sed -i "/deb-src/s/# //g" /etc/apt/sources.list
 
 # install dep
 apt update
-apt-get install -y wget xz-utils make gcc flex bison dpkg-dev bc rsync kmod cpio libssl-dev
+apt install bc bison build-essential fakeroot flex git libelf-dev libssl-dev ncurses-dev rsync wget xz-utils -y
 apt build-dep -y linux
 
 # change dir to workplace
