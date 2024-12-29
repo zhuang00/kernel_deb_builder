e1f530909aea847a46ac77f45329285a60d2c29a
zhuang00 <zhuang2011@gmail.com>
2024-12-29 23:10:42 +0800

    Update build_action.sh

--- build_action.sh
+++ build_action.sh
@@ -22,7 +22,13 @@ cp ../config .config
 # disable DEBUG_INFO to speedup build
 
 mk-build-deps --install
-scripts/config --disable DEBUG_INFO
+scripts/config --undefine GDB_SCRIPTS
+scripts/config --undefine DEBUG_INFO
+scripts/config --undefine DEBUG_INFO_SPLIT
+scripts/config --undefine DEBUG_INFO_REDUCED
+scripts/config --undefine DEBUG_INFO_COMPRESSED
+scripts/config --set-val  DEBUG_INFO_NONE       y
+scripts/config --set-val  DEBUG_INFO_DWARF5     n
 scripts/config --disable DEBUG_INFO_DWARF_TOOLCHAIN_DEFAULT
 
 
